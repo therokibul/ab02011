3a68e0d94d877013ddbfcc5982cde08a0ab38dcf
therokibul <therokibul@gmail.com>
2021-10-26 12:49:45 +0600

    26-10

--- lib/first_page.dart
+++ lib/first_page.dart
@@ -8,17 +8,95 @@ class FirstPage extends StatefulWidget {
   _FirstPageState createState() => _FirstPageState();
 }
 
+String name = '';
+bool show = true;
+bool changeButton = false;
+
 class _FirstPageState extends State<FirstPage> {
   @override
   Widget build(BuildContext context) {
-    return Material(
-      child: Container(
-        color: Colors.green,
-        child: MaterialButton(
-          onPressed: () {
-            Navigator.pushNamed(context, MyRoute.secondPage);
-          },
-          child: Text('Go to Second Page'),
+    return SafeArea(
+      child: Material(
+        child: Center(
+          child: Column(
+            // mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                'Log in',
+                style: TextStyle(fontSize: 40),
+              ),
+              SizedBox(
+                height: 200,
+              ),
+              Text(
+                'Welcome $name',
+                style: TextStyle(fontSize: 40),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: TextField(
+                  decoration: InputDecoration(
+                    hintText: 'Enter Your Name',
+                    labelText: 'Name',
+                  ),
+                  onChanged: (value) {
+                    setState(() {
+                      name = value;
+                    });
+                  },
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: TextField(
+                  obscureText: show,
+                  decoration: InputDecoration(
+                    hintText: 'Password',
+                    labelText: 'Password',
+                    suffix: TextButton(
+                      onPressed: () {
+                        setState(() {
+                          show = !show;
+                        });
+                      },
+                      child: show ? Text('Show') : Text('Hide'),
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 100,
+              ),
+              InkWell(
+                onTap: () async {
+                  setState(() {
+                    changeButton = !changeButton;
+                  });
+                  await Future.delayed(Duration(seconds: 2));
+                  Navigator.pushNamed(context, MyRoute.secondPage);
+                },
+                child: AnimatedContainer(
+                  duration: Duration(seconds: 2),
+                  width: changeButton ? 50 : 300,
+                  padding: EdgeInsets.all(10),
+                  child: changeButton
+                      ? Icon(Icons.done_outlined)
+                      : Text(
+                          'Login',
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                            fontSize: 30,
+                            color: Colors.white,
+                          ),
+                        ),
+                  decoration: BoxDecoration(
+                      color: Colors.green,
+                      borderRadius:
+                          BorderRadius.circular(changeButton ? 30 : 50)),
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,7 @@ import 'second_page.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatefulWidget {
-  const MyApp({ Key? key }) : super(key: key);
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   _MyAppState createState() => _MyAppState();
@@ -15,9 +15,10 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      
       initialRoute: "/",
       routes: {
-        "/": (context) => FirstPage(), 
+        "/": (context) => FirstPage(),
         MyRoute.firstPage: (contex) => FirstPage(),
         MyRoute.secondPage: (contex) => SecondPage(),
       },
@@ -25,7 +26,7 @@ class _MyAppState extends State<MyApp> {
   }
 }
 
-class MyRoute{
+class MyRoute {
   static String firstPage = '/first';
   static String secondPage = '/second';
 }
