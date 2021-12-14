65731ecd3640a3fa01c7d9cc8fdece888f4b9a19
therokibul <therokibul@gmail.com>
2021-12-14 11:09:00 +0600

    http

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,64 @@
-import 'dart:js';
-
-import 'package:ab02011/views/home_desktop.dart';
-import 'package:ab02011/views/home_mobile.dart';
+import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'models/model_data.dart';
 
-void main() => runApp(MaterialApp(
-      home: Home(),
-    ));
+void main() => runApp(MyApp());
 
-class Home extends StatelessWidget {
-  const Home({Key? key}) : super(key: key);
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: LayoutBuilder(
-        builder: (BuildContext context, BoxConstraints constraints) {
-      if (constraints.maxWidth < 600) {
-        return HomeMobile();
-      } else {
-        return HomeDesktop();
+    return MaterialApp(
+      home: Scaffold(
+        body: HttpTest(),
+      ),
+    );
+  }
+}
+
+class HttpTest extends StatelessWidget {
+  const HttpTest({Key? key}) : super(key: key);
+
+  Future getData() async {
+    var response =
+        await http.get(Uri.parse('https://jsonplaceholder.typicode.com/users'));
+
+    var responseData = jsonDecode(response.body);
+    List users = [];
+    for (var index in responseData) {
+      User user = User(
+          name: index['name'],
+          username: index['username'],
+          email: index['email'],
+          phone: index['phone']);
+          users.add(user);
+    }
+    return users;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+      future: getData(),
+      builder: (BuildContext context , AsyncSnapshot snapshot){
+        if(snapshot.data == null){
+          return CircularProgressIndicator();
+        }else{
+          return ListView.builder(
+            itemCount: snapshot.data.length,
+            
+            itemBuilder: (context, index){
+              return Text(snapshot.data[index].name);
+            }
+            
+            );
+        }
+
       }
-    }));
+      
+      
+      );
   }
 }
