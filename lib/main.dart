0ac5ea90aeb6e9843fe4c69aef67e49730f98e8a
therokibul <therokibul@gmail.com>
2022-01-04 12:14:17 +0600

    z

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,15 @@
 import 'package:ab02011/pages/home.dart';
 import 'package:ab02011/pages/login.dart';
+import 'package:firebase_core/firebase_core.dart';
 
 
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+   runApp(MyApp());
+   }
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
